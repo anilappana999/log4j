4063613b6a8c5bb769856dadddce705dc5015e2a
anilappana999 <anilappana999@gmail.com>
2021-04-05 16:12:23 +0530

    code-commit for log4j

---
+++ MyFirstLog4jApp/src/App.java
@@ -0,0 +1,12 @@
+import org.apache.log4j.Logger;
+
+class App{
+	
+	
+	static Logger log = Logger.getLogger(App.class.getName());
+	public static void main(String[] args) {
+		
+		log.info("welcome to my channel");
+		
+	}
+}
